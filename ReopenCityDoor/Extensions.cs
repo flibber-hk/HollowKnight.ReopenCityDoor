7423fbd8f21d1d918ff7a76a668081ecf11e833b
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2022-02-12 16:31:03 +0000

    Initial commit

---
+++ ReopenCityDoor/Extensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ReopenCityDoor
+{
+    public static class Extensions
+    {
+        public static bool DestroyRootGameObject(this Scene scene, string objectName)
+        {
+            foreach (GameObject go in scene.GetRootGameObjects())
+            {
+                if (go.name == objectName)
+                {
+                    UnityEngine.Object.Destroy(go);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static GameObject CreateTransition(Vector2 bl, Vector2 tr, string gateName, string targetScene, string targetGate)
+        {
+            GameObject transitionGO = new GameObject();
+            transitionGO.name = gateName;
+            transitionGO.transform.position = (bl + tr) / 2;
+            
+            BoxCollider2D box = transitionGO.AddComponent<BoxCollider2D>();
+            box.size = tr - bl;
+            box.offset = Vector2.zero;
+            box.isTrigger = true;
+
+            TransitionPoint tp = transitionGO.AddComponent<TransitionPoint>();
+            tp.targetScene = targetScene;
+            tp.entryPoint = targetGate;
+            tp.nonHazardGate = true;
+
+            GateSnap snap = transitionGO.AddComponent<GateSnap>();
+
+            transitionGO.SetActive(true);
+            return transitionGO;
+        }
+    }
+}
---
+++ ReopenCityDoor/ReopenCityDoor.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Modding;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ReopenCityDoor
+{
+    public class ReopenCityDoor : Mod
+    {
+        internal static ReopenCityDoor instance;
+
+        /// <summary>
+        /// Override whether the gate should be open.
+        /// Input: false on Fungal side, True on City side
+        /// Output: False to leave the gate closed
+        /// </summary>
+        public static event Func<bool, bool> ShouldOpenGate;
+        internal static bool GetShouldOpenGate(bool citySide)
+        {
+            return ShouldOpenGate?.Invoke(citySide) ?? true;
+        }
+
+        public ReopenCityDoor() : base(null)
+        {
+            instance = this;
+        }
+        
+        public override string GetVersion()
+        {
+            return GetType().Assembly.GetName().Version.ToString();
+        }
+        
+        public override void Initialize()
+        {
+            Log("Initializing Mod...");
+
+            UnityEngine.SceneManagement.SceneManager.activeSceneChanged += OpenGate;
+        }
+
+        private void OpenGate(Scene oldScene, Scene newScene)
+        {
+            switch (newScene.name)
+            {
+                case "Fungus2_08" when GetShouldOpenGate(false) || GameManager.instance.entryGateName == "rightRCD":
+                    OpenFungalGate(newScene);
+                    break;
+                case "Crossroads_49b" when GetShouldOpenGate(true) || GameManager.instance.entryGateName == "leftRCD":
+                    OpenCityGate(newScene);
+                    break;
+            }
+        }
+
+        private void OpenFungalGate(Scene scene)
+        {
+            scene.DestroyRootGameObject("Ruins_front_gate");
+            scene.DestroyRootGameObject("Ruins_gate_0004_a");
+            Extensions.CreateTransition(new(30, 58), new(31, 66), "rightRCD", "Crossroads_49b", "leftRCD");
+            Log(HeroController.instance.transform.position);
+        }
+
+        private void OpenCityGate(Scene scene)
+        {
+            scene.DestroyRootGameObject("Ruins_front_gate");
+            Extensions.CreateTransition(new(-1, 6), new(0, 14), "leftRCD", "Fungus2_08", "rightRCD");
+            Log(HeroController.instance.transform.position);
+        }
+    }
+}
